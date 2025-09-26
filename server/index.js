54bbc456bc8c8c95b112d81c660049c20c630479
Aatti13 <86865554+Aatti13@users.noreply.github.com>
2025-09-26 11:58:04 +0530

    Completed v1-server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
+
+import config from './src/config/index.js';
 
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT;
+const PORT = config.port;
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.listen(PORT, ()=>{
   console.log(`Server Listening at: http://localhost:${PORT}`);
